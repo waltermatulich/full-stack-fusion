5001e1970db636325b0c1abe9a077604b8db4d2f
waltermatulich <wmatulich231@gmail.com>
2024-03-05 09:38:56 -0800

    modified connection

--- back-end/config/connection.js
+++ back-end/config/connection.js
@@ -1,61 +1,7 @@
 const mongoose = require('mongoose');
 
-// Environment variable for database URI
-const dbURI = process.env.MONGODB_URI || 'your_default_local_db_connection_string';
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-};
-
-// Establishing connection to MongoDB
-mongoose.connect(dbURI, options).then(
-  () => { console.log("Database connection established!"); },
-  err => { console.log("Error connecting Database instance due to: ", err); }
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-thoughts'
 );
 
-// Handling connection events
-mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
-});
-
-mongoose.connection.on('error', err => {
-  console.log(`Mongoose connection error: ${err}`);
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
-});
-
-// To close the Mongoose connection when the application ends
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close(() => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
-};
-
-// For nodemon restarts
-process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart', () => {
-    process.kill(process.pid, 'SIGUSR2');
-  });
-});
-
-// For app termination
-process.on('SIGINT', () => {
-  gracefulShutdown('app termination', () => {
-    process.exit(0);
-  });
-});
-
-// For Heroku app termination
-process.on('SIGTERM', () => {
-  gracefulShutdown('Heroku app shutdown', () => {
-    process.exit(0);
-  });
-});
-
-module.exports = mongoose;
+module.exports = mongoose.connection;

dbe73b688a1468f2c69ddb5ffd26f443cb0bcf69
waltermatulich <wmatulich231@gmail.com>
2024-03-05 13:38:07 -0800

    modified schema js files

---
+++ back-end/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { } = require("../models");
+const { signToken, authenticationError } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+
+    }
+}
+
+module.exports = resolvers;

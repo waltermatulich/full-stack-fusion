c885f4584ef16b6dda5a0ad3e7b1439764abd3ce
waltermatulich <wmatulich231@gmail.com>
2024-03-05 14:52:29 -0800

    js file updates

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, routerProvider } from "react-router-dom/dist"
-
+import Home from "./pages/Home.jsx"
 const router = createBrowserRouter({
   path: "/",
   elements: <App />,
   children: [
     {
       index: true,
-      element: < App />
+      element: < Home />
     }
   ]
 })

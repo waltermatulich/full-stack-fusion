c22647c43da4088297e1e928b6a6e42a5e6ba8c7
waltermatulich <wmatulich231@gmail.com>
2024-03-05 09:43:49 -0800

    updated database

--- back-end/config/connection.js
+++ back-end/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-thoughts'
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fusion-db'
 );
 
 module.exports = mongoose.connection;

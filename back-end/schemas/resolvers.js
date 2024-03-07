50c0c66d36cab325df00906f8526733e15ee0482
Mandeep Singh <mandeepsingh@mandeeps-air.lan>
2024-03-07 14:50:15 -0800

    Got back-end to work. Added mutations, fixed some code in server.j s

--- back-end/schemas/resolvers.js
+++ back-end/schemas/resolvers.js
@@ -1,17 +1,26 @@
 const { User } = require("../models");
-const { signToken, authenticationError } = require("../utils/auth");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-        users: async(parent, args) => {
-            return await User.find({
-
-            })
-        }
+        users: async () => {
+            try {
+                return await User.find();
+            } catch (error) {
+                throw new Error("Failed to fetch users");
+            }
+        },
     },
-    // Mutation: {
-
-    // }
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            try {
+                const user = await User.create({ username, email, password });
+                return user;
+            } catch (error) {
+                throw new Error("Failed to create user");
+            }
+        },
+    }
 }
 
 module.exports = resolvers;
--- back-end/utils/auth.js
+++ back-end/utils/auth.js
@@ -5,34 +5,40 @@ require("dotenv").config();
 const secret = process.env.JWT_SECRET || "mysecretpasphrase";
 const expiration = '30m';
 
-module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+const AuthenticationError = new GraphQLError('Could not authenticate user.', {
+  extensions: {
+    code: 'UNAUTHENTICATED',
+  },
+});
 
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+const authMiddleware = function ({ req }) {
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (!token) {
-      return req;
-    }
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
 
-    try {
-      const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = authenticatedPerson;
-    } catch {
-      console.log('Invalid token');
-    }
-
+  if (!token) {
     return req;
-  },
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
-    return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
-  },
+  }
+
+  try {
+    const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = authenticatedPerson;
+  } catch {
+    console.log('Invalid token');
+  }
+
+  return req;
+};
+
+const signToken = function ({ email, username, _id }) {
+  const payload = { email, username, _id };
+  return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
+};
+
+module.exports = {
+  signToken,
+  AuthenticationError,
+  authMiddleware
 };

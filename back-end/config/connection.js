303d77a97bb780eac6bc69e5a739656cac9691a4
waltermatulich <wmatulich231@gmail.com>
2024-03-08 11:21:19 -0800

    fixed mongo connection

--- back-end/config/connection.js
+++ back-end/config/connection.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fusion-db',
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    
   }
 );
 

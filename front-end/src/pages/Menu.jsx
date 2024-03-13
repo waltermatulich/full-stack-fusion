c65c4efab3518288b6d99059fa3ef2f69a601c93
elimendo <emendoza8361@gmail.com>
2024-03-12 18:26:36 -0700

    login

--- front-end/src/pages/Menu.jsx
+++ front-end/src/pages/Menu.jsx
@@ -4,10 +4,10 @@ import Nav from "../components/Nav";
 import logo from "../assets/logo.png";
 import menuPhoto from "../assets/menuPhoto.jpg";
 import menuBurger from "../assets/menuBurger.jpg";
-import mussels from '../assets/mussels.jpg';
-import pickles from '../assets/pickles.jpg';
-import alfredo from '../assets/alfredo.jpg'
-import menu from "../assets/menu.png"
+import mussels from "../assets/mussels.jpg";
+import pickles from "../assets/pickles.jpg";
+import alfredo from "../assets/alfredo.jpg";
+import menu from "../assets/menu.png";
 
 function Menu() {
   return (
@@ -21,63 +21,86 @@ function Menu() {
         <img src={menuPhoto}></img>
       </div>
       <Nav />
-
+      <section>
       <div className="milkyMenu">
-      <img src={menu}></img>
+        <img src={menu}></img>
       </div>
 
+      <a target="_blank" href="https://buy.stripe.com/test_fZeeYp5g01CleLS288">
         <div className="mannyMussels">
-            <h2>Manny's Mussels   $18</h2>
+          <h2>Manny's Mussels $18</h2>
         </div>
 
-      <div className="mussels">
-        <img src={mussels}></img>
-      </div>
+        <div className="mussels">
+          <img src={mussels}></img>
+        </div>
 
-      <div className="musselsDesc">
-        <p> Manny's Mussels, steamed to tender perfection and served with our renowned Manny Muscle Sauce. </p>
-      </div>
+        <div className="musselsDesc">
+          <p>
+            {" "}
+            Manny's Mussels, steamed to tender perfection{" "}
+          </p>
+          <p>
+          and served with our
+            renowned Manny Muscle Sauce.
+          </p>
+        </div>
+      </a>
 
-      <div className="bodBurger">
-        <h2>Bodacious Burger $15</h2>
-      </div>
+      <a target="_blank" href="https://buy.stripe.com/test_8wMaI923O2Gp9ry9AB">
+        <div className="bodBurger">
+          <h2>Bodacious Burger $15</h2>
+        </div>
 
-      <div className="menuBurger">
-        <img src={menuBurger}></img>
-      </div>
+        <div className="menuBurger">
+          <img src={menuBurger}></img>
+        </div>
 
-      <div className="bodBurgDesc">
-        <p> Bodacious Burger. Sink your teeth into a perfectly seasoned beef patty, enhanced by our delectable Muscle Sauce </p>
-      </div>
+        <div className="bodBurgDesc">
+          <p>
+            {" "}
+            Bodacious Burger. Sink your teeth into a perfectly seasoned beef
+            patty, enhanced by our delectable Muscle Sauce{" "}
+          </p>
+        </div>
+      </a>
 
-      <div className="plump">
-        <h2>Plump Pickles $4</h2>
-      </div>
+      <a target="_blank" href="https://buy.stripe.com/test_5kA6rTfUEa8ReLS6oq">
+        <div className="plump">
+          <h2>Plump Pickles $4</h2>
+        </div>
 
-      <div className="pickles">
-        <img src={pickles}></img>
-      </div>
+        <div className="pickles">
+          <img src={pickles}></img>
+        </div>
 
-      <div className="pickleDesc">
-        <p> Satisfy your cravings with our Plump Pickles. Pickled to perfection and paired with our signature Muscle Sauce. </p>
-      </div>
+        <div className="pickleDesc">
+          <p>
+            {" "}
+            Satisfy your cravings with our Plump Pickles. Pickled to perfection
+            and paired with our signature Muscle Sauce.{" "}
+          </p>
+        </div>
+      </a>
+      <a target="_blank" href="https://buy.stripe.com/test_eVa03veQA80J5bidQT">
+        <div className="alfredo">
+          <h2>Alfredo $17</h2>
+        </div>
 
-      <div className="alfredo">
-        <h2>Alfredo $17</h2>
-      </div>
+        <div className="alfredoImg">
+          <img src={alfredo}></img>
+        </div>
 
-      <div className="alfredoImg">
-        <img src={alfredo}></img>
-      </div>
-
-      <div className="alfredoDisc">
-        <p>  Our Alfredo Pasta enriched with the irresistible flavor of our signature Muscle Sauce. Our creamy Alfredo sauce, delicately crafted, perfectly complements the rich notes of the Muscle Sauce.</p>
-      </div>
-
-
-
-
-    
+        <div className="alfredoDisc">
+          <p>
+            {" "}
+            Our Alfredo Pasta enriched with the irresistible flavor of our
+            signature Muscle Sauce. Our creamy Alfredo sauce, delicately
+            crafted, perfectly complements the rich notes of the Muscle Sauce.
+          </p>
+        </div>
+      </a>
+      </section>
     </div>
   );
 }
--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -1,22 +1,38 @@
-
-
+import Nav from "../components/Nav";
+import { Link } from "react-router-dom";
+import login from "../assets/backgroundLogin.jpg";
+import "../pages/css/signup.css";
 function Signup(){
     return(
         <div>
-            <h1>Sign Up</h1>
-            <form>
-        <label for="fname">First name:</label>
-        <br />
-        <input type="text" required id="fname" name="fname" />
-        <br />
-        <label for="emaills">Email:</label>
-        <br />
-        <input type="email" required id="email" name="emaills" />
-        <label for='message'></label>
-        <br/>
-        <button type="submit">Submit</button>
-      </form>
-        </div>
+      <Nav />
+      <img className="loginImg" src={login} alt="Login Background" />
+
+      <div className="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input-box">
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" />
+          </div>
+          
+
+            <button type="submit">Sign Up</button>
+
+            <div className="register-link">
+              <p>
+                Create your muscle account!
+              </p>
+            </div>
+          
+        </form>
+      </div>
+    </div>
     )
 }
 export default Signup

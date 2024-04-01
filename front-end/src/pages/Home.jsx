238aff808d908059aeae169fcf691269a8f0ca6d
elimendo <emendoza8361@gmail.com>
2024-04-01 15:41:31 -0700

    home page adjustments

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import foodVid from '../assets/food-vid.mp4'
 import { Link } from "react-router-dom";
 import Nav from '../components/Nav';
 import frontMussels from "../assets/frontmussels.jpg"
+import onlineOrder from "../assets/onlineOrder.jpg"
+import staffImg from "../assets/staffImg.jpg"
 const Home = () => {
   return (
     <div>
@@ -27,15 +29,29 @@ const Home = () => {
         {/* <button className="btn"> Order Now</button> */}
         </Link>
       </div>
+      {/* For the three photos below the video home page */}
       <div className="welcome">
-        <h1> Welcome to Manny Munchies</h1>
-        <div className="frontMussels">
-          <img src={frontMussels}></img>
-          <h1>
-            Try our Signature Mussels!</h1>
+    <h1> Welcome to Manny Munchies</h1>
+    <div className="frontMussels">
+        <div className="imageContainer">
+            <img src={staffImg} alt="Online Order"></img>
+            <h1>Meet the Staff</h1>
+            
+        </div>
+        <div className="imageContainer">
+            <img src={frontMussels} alt="Front Mussels"></img>
+            <h1>Try our Signature Mussels!</h1>
             <h2>A house favorite</h2>
         </div>
-      </div>
+        <div className="imageContainer">
+            <img src={onlineOrder} alt="Online Order"></img>
+            <h1>Order Online</h1>
+            
+        </div>
+    </div>
+</div>
+
+      
     </div>
   );
 };

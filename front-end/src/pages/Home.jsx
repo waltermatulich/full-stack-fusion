4f03886df4c477bab0265bd78395b49304190667
elimendo <emendoza8361@gmail.com>
2024-04-01 13:21:48 -0700

    home page progress

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import "./home.css";
 import logo from "../assets/logo.png";
 import foodVid from '../assets/food-vid.mp4'
 import { Link } from "react-router-dom";
+import Nav from '../components/Nav';
+import frontMussels from "../assets/frontmussels.jpg"
 const Home = () => {
   return (
     <div>
@@ -10,7 +12,7 @@ const Home = () => {
           <Link to='/' className="logo">
           <img src={logo}></img>
           </Link>
-          
+          <Nav />
         
       </div>
       
@@ -21,10 +23,18 @@ const Home = () => {
         </video>
       </div>
       <div className="content">
-        <h1>Hungry?</h1>
         <Link to='/menu'>
-        <button className="btn"> Order Now</button>
+        {/* <button className="btn"> Order Now</button> */}
         </Link>
+      </div>
+      <div className="welcome">
+        <h1> Welcome to Manny Munchies</h1>
+        <div className="frontMussels">
+          <img src={frontMussels}></img>
+          <h1>
+            Try our Signature Mussels!</h1>
+            <h2>A house favorite</h2>
+        </div>
       </div>
     </div>
   );
